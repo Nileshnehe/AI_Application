0124d5b8781f4887a066058b87f6d38e482957ce
Nileshnhe <nehenilesh111@gmail.com>
2026-09-12 10:40:31 +0530

    feat: add Redis ping check during server startup

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,11 +1,13 @@
 import app from "./app";
 import { connectDB } from "./config/db";
 import { ENV } from "./config/env";
+import redisClient from "./config/redis";
 
 
-const startServer = async ()  => {
+const startServer = async () => {
     try {
         await connectDB();
+        await redisClient.ping();
 
         app.listen(ENV.PORT, () => {
             console.log(`Production server running on port ${ENV.PORT}`)
