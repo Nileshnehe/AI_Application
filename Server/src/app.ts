25a136c7a11d997d7cf9d0817f4874287d4df144
Nileshnhe <nehenilesh111@gmail.com>
2026-09-17 22:27:47 +0530

    feat: refactor app setup by organizing middleware and improving code readability

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,17 +1,37 @@
 import express from 'express';
 import helmet from 'helmet';
-import cors from 'cors';
+// import cors from 'cors';
 import { ENV } from './config/env';
+import { authLimiter } from './middlewares/rateLimiter';
+import router from './routes';
+import { errorHandler } from './middlewares/error.middleware';
+import cookieParser from 'cookie-parser';
+import { pinoHttp } from 'pino-http';
 
-const app=express();
+const app = express();
 
 app.use(helmet());
+// app.use(
+//     cors({
+//         origin: ENV.CLIENT_URL,
+//         credentials: true,
+//     })
+// );
+
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use(
-    cors({
-        origin: 'ENV_CLIENT_URL',
-        credentials: true,
+    pinoHttp({
+        transport: ENV.NODE_ENV !== 'production' ? { target: 'pino-pretty'} : undefined,
     })
 );
+
+// app.use('/api', authLimiter);
+app.use('/api/v1', router);
+
+app.use(errorHandler);
 
 
 
