683d66315000ec9c4a267e2a4ed1a3e985119bbc
Nileshnhe <nehenilesh111@gmail.com>
2026-09-18 16:39:42 +0530

    feat: add forgot password and reset password routes, update verify email route method

--- Server/src/routes/auth.route.ts
+++ Server/src/routes/auth.route.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { AuthController, loginSchema, registerSchema, verifyEmailSchema } from "../controllers/auth.controller";
+import { AuthController, forgotPasswordSchema, loginSchema, registerSchema, resetPasswordSchema, verifyEmailSchema } from "../controllers/auth.controller";
 // import { authLimiter } from "../middlewares/rateLimiter";
 import { validateRequest } from "../middlewares/validate.middleware";
 
 const authRouter = Router();
 // i will remove the authLimiter for testing purpose
 authRouter.post('/register', validateRequest(registerSchema), AuthController.register);
-authRouter.post('/verify-email', validateRequest(verifyEmailSchema), AuthController.verifyEmail);
+authRouter.get('/verify-email', validateRequest(verifyEmailSchema), AuthController.verifyEmail);
 authRouter.post('/login', validateRequest(loginSchema), AuthController.login);
-
+authRouter.post('/forgot-password', validateRequest(forgotPasswordSchema), AuthController.forgotPassword);
+authRouter.post('/reset-password', validateRequest(resetPasswordSchema), AuthController.resetPassword);
 
 export default authRouter;
